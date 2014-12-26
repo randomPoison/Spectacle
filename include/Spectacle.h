45fe55118d1142d92057c8e2054299be8a467bb2
David LeGare <excaliburhissheath@gmail.com>
2014-12-26 17:17:36 -0600

    Update to Gunship v0.2.0 Development
    - Setup Spectacle to use the latest version of Gunship.

--- include/Spectacle.h
+++ include/Spectacle.h
@@ -1,8 +1,14 @@
 #pragma once
 
-#include <Gunship.h>
+#include <Engine.h>
+#include <Scene.h>
+#include <System.h>
+#include <Components/Transform.h>
+#include <Components/Camera.h>
+#include <Components/Mesh.h>
+#include <Components/Light.h>
 
-void MakePlayer( GameObject& player, GameObject& target );
-void MakeCamera( GameObject& camera, GameObject& cameraAnchor, GameObject& player );
-void MakeBullet( GameObject bullet, Ogre::Vector3 start, Ogre::Vector3 dir );
-void MakeEnemy( GameObject enemy, GameObject& player, Ogre::Vector3 start);
+//void MakePlayer( GameObject& player, GameObject& target );
+//void MakeCamera( GameObject& camera, GameObject& cameraAnchor, GameObject& player );
+//void MakeBullet( GameObject bullet, Ogre::Vector3 start, Ogre::Vector3 dir );
+//void MakeEnemy( GameObject enemy, GameObject& player, Ogre::Vector3 start);
--- source/Spectacle.cpp
+++ source/Spectacle.cpp
@@ -2,29 +2,128 @@
 
 #include "Spectacle.h"
 
-int main( int argc, char* argv[] )
+struct Player : public entityx::Component< Player >
 {
-	Gunship engine;
-	if ( !engine.InitSystems() )
+	float moveSpeed;
+
+	Player( float speed = 1.0f )
+		: moveSpeed( speed )
 	{
-		std::cout << "Could not initialize rendering systems!" << std::endl;
-		return 1;
+	}
+};
+
+struct FollowTarget : public entityx::Component< FollowTarget >
+{
+	Gunship::Transform::Handle targetTransform;
+	Ogre::Vector3 offset;
+	float followSpeed;
+
+	FollowTarget( Gunship::Transform::Handle target,
+	              Ogre::Vector3 offset = Ogre::Vector3::ZERO,
+	              float followSpeed = 10.0f )
+		: targetTransform( target ),
+		  offset( offset ),
+		  followSpeed( followSpeed )
+	{
 	}
 
-	Scene* scene = engine.CurrentScene();
+	Ogre::Vector3 desiredPosition()
+	{
+		return targetTransform->node->_getDerivedPosition() + offset;
+	}
+};
 
-	GameObject player = scene->AddGameObject( "Player" );
-	GameObject target = scene->AddGameObject( "Target" );
-	GameObject camera = scene->AddGameObject( "Camera" );
-	GameObject cameraAnchor = scene->AddGameObject( "CameraAnchor" );
+struct PlayerMovementSystem : public Gunship::System< PlayerMovementSystem, Gunship::BehaviorSystemBase >
+{
+	void Update( entityx::EntityManager& entities,
+	             float delta ) override
+	{
+		Gunship::Transform::Handle transform;
+		Player::Handle player;
+		for ( entityx::Entity entity : entities.entities_with_components( transform, player ) )
+		{
+			if ( Gunship::Input::KeyDown( SDL_SCANCODE_D ) )
+			{
+				transform->node->translate( Ogre::Vector3::UNIT_X * player->moveSpeed * delta );
+			}
+			if ( Gunship::Input::KeyDown( SDL_SCANCODE_A ) )
+			{
+				transform->node->translate( Ogre::Vector3::NEGATIVE_UNIT_X * player->moveSpeed * delta );
+			}
+			if ( Gunship::Input::KeyDown( SDL_SCANCODE_W ) )
+			{
+				transform->node->translate( Ogre::Vector3::UNIT_Y * player->moveSpeed * delta );
+			}
+			if ( Gunship::Input::KeyDown( SDL_SCANCODE_S ) )
+			{
+				transform->node->translate( Ogre::Vector3::NEGATIVE_UNIT_Y * player->moveSpeed * delta );
+			}
 
-	MakePlayer( player, target );
-	MakeCamera( camera, cameraAnchor, player );
+			transform->node->_getDerivedPositionUpdated();
+		}
+	}
+};
 
-	MakeEnemy( scene->AddGameObject(), player, Ogre::Vector3( 10.0f, 10.0f, 0.0f ) );
+struct CameraFollowSystem : public Gunship::System< CameraFollowSystem, Gunship::BehaviorSystemBase >
+{
+	void Update( entityx::EntityManager& entities,
+	             float delta ) override
+	{
+		FollowTarget::Handle followTarget;
+		Gunship::Transform::Handle transform;
+		for ( entityx::Entity entity : entities.entities_with_components( followTarget, transform ) )
+		{
+			Ogre::Vector3 desiredPostition = followTarget->desiredPosition();
+			Ogre::Vector3 resultPosition = Ogre::Math::lerp( transform->node->_getDerivedPosition(),
+			                                                 desiredPostition,
+			                                                 followTarget->followSpeed * delta );
+			transform->node->_setDerivedPosition( resultPosition );
+		}
+	}
+};
 
-	engine.Start();
-	engine.ShutDown();
+void InitializeScene( Gunship::Scene& scene )
+{
+	// add systems to scene
+	scene.AddSystem< PlayerMovementSystem >();
+	scene.AddSystem< CameraFollowSystem >();
+
+	entityx::Entity camera = scene.CreateGameObject();
+	Gunship::Transform::Handle cameraTransform = camera.assign<
+		Gunship::Transform >( scene );
+	camera.assign< Gunship::Camera >( scene, cameraTransform );
+	cameraTransform->node->setPosition( 0.0f, 0.0f, 30.0f );
+	cameraTransform->node->_getDerivedPositionUpdated(); // trigger ogre to recalculate derived position.
+	cameraTransform->node->lookAt( Ogre::Vector3( 0.0f, 0.0f, 0.0f ),
+	                               Ogre::Node::TS_WORLD );
+
+	entityx::Entity cube = scene.CreateGameObject();
+	Gunship::Transform::Handle cubeTransform =
+		cube.assign< Gunship::Transform >( scene );
+	cubeTransform->node->_getDerivedPositionUpdated();
+	cube.assign< Gunship::Mesh >( scene, cubeTransform, "Cube.mesh" );
+	cube.assign< Player >( 10.0f );
+
+	camera.assign< FollowTarget >( cubeTransform, Ogre::Vector3( 0.0f, 0.0f, 30.0f ), 5.0f );
+
+	entityx::Entity light = scene.CreateGameObject();
+	Gunship::Transform::Handle lightTransform =
+		light.assign< Gunship::Transform >( scene );
+	lightTransform->node->setPosition( 12.0f, 56.0f, 3.0f );
+	light.assign< Gunship::Components::Light >( scene, lightTransform );
+}
+
+int main( int argc, char* argv[] )
+{
+	Gunship::Engine engine;
+
+	// initialize and run the engine
+	if ( engine.InitSystems() )
+	{
+		engine.SetupCurrentScene( InitializeScene );
+		engine.Start();
+		engine.ShutDown();
+	}
 
 	return 0;
 }
