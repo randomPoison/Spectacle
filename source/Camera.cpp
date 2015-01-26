0d516f6cdc36572216e28734e15b23109f15be57
David LeGare <excaliburhissheath@gmail.com>
2015-01-25 22:53:37 -0600

    Create Camera
    - Creating a camera in the scene now works again.

--- source/Camera.cpp
+++ source/Camera.cpp
@@ -1,5 +1,6 @@
 #include <Scene.h>
 #include <Components/Camera.h>
+#include <Components/SimpleStructComponentManager.h>
 
 #include "Camera.h"
 #include "Components/FollowTargetLinear.h"
@@ -9,14 +10,16 @@ namespace Spectacle
 	Gunship::Entity CreateCamera( Gunship::Scene& scene, Gunship::Entity player )
 	{
 		Gunship::Entity camera = scene.entities().Create();
+
+//		camera.assign< Gunship::Components::Transform >();
+//		camera.assign< Gunship::Components::Camera >();
+
+		scene.componentManager< Gunship::Components::TransformManager >().Assign( camera.id );
+		scene.componentManager< Gunship::Components::CameraManager >().Assign( camera.id );
+
+//		cameraTransform->SetPosition( 0.0f, 0.0f, 30.0f );
+//		cameraTransform->LookAt( Gunship::Vector3( 0.0f, 0.0f, 0.0f ) );
 /*
-		Gunship::Components::Transform::Handle cameraTransform =
-			camera.assign< Gunship::Components::Transform >( scene );
-		camera.assign< Gunship::Components::Camera >( scene, cameraTransform );
-		cameraTransform->SetPosition( 0.0f, 0.0f, 30.0f );
-
-		cameraTransform->LookAt( Gunship::Vector3( 0.0f, 0.0f, 0.0f ) );
-
 		Gunship::Components::Transform::Handle playerTransform = player.component< Gunship::Components::Transform >();
 		camera.assign< FollowTargetLinear >( playerTransform, Gunship::Vector3( 0.0f, 0.0f, 30.0f ), 5.0f );
 */
